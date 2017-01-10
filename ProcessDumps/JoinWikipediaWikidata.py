f2bab72e332136de42e1e26c26f73bd963907d1b
BuildTools <tiziano.piccardi@epfl.ch>
2017-01-10 19:51:13 +0100

    join wikidata: working version

--- ProcessDumps/JoinWikipediaWikidata.py
+++ ProcessDumps/JoinWikipediaWikidata.py
@@ -15,4 +15,47 @@ sc = SparkContext()
 sqlContext = SQLContext(sc)
 sqlContext.setConf("spark.sql.parquet.compression.codec", "snappy")
 
-df = sqlContext.read.parquet('hdfs:///user/piccardi/parquet/merged_sections.parquet')
+white_list = ["itwiki", "enwiki", "dewiki", "fawiki", "nlwiki", "frwiki", "eswiki"]
+
+links = sqlContext.read.parquet('hdfs:///user/piccardi/parquet/wikidata_links.parquet')
+
+def pivot_row(row):
+    result = []
+    for l in white_list:
+        result.append(Row(id=row.id, lang=l, title=row[l]))
+    return result
+
+# DataFrame[id: string, lang: string, title: string]
+titles = sqlContext.createDataFrame(links.flatMap(pivot_row))
+titles.registerTempTable("titles")
+###############################################à
+
+# merged_sections => DataFrame[id: bigint, origin: string, sections: array<string>, title: string]
+sections_join = sqlContext.sql("""
+    SELECT all_sections.*, titles.id as wikidata_id
+    FROM parquet.`hdfs:///user/piccardi/parquet/merged_sections.parquet` as all_sections
+    JOIN titles
+    ON all_sections.origin = titles.lang AND all_sections.title = titles.title
+""")
+
+"""
+[Row(id=1277775, origin='dewiki', sections=['SIEHE AUCH', 'EINZELNACHWEISE'],
+title='(1016) Anitra', wikidata_id='Q11553'), Row(id=7952160, origin='dewiki',
+sections=['WEBLINKS', 'EINZELNACHWEISE'], title='(11335) Santiago', wikidata_id='Q370943')
+...]
+"""
+
+# group by wikidata id
+def map_sections(row):
+    labeled_titles = [row.origin+":"+t for t in row.sections]
+    return (row.wikidata_id, labeled_titles)
+
+def merge_sections(a, b):
+    return a+b
+
+merged_sections = sections_join.map(map_sections).reduceByKey(merge_sections)
+
+def as_string(tpl):
+    return "\t".join(tpl[1])
+
+merged_sections.map(as_string).saveAsTextFile('hdfs:///user/piccardi/all_sections_merged.txt')
